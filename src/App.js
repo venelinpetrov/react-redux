514767bb207e827369f21129181fa71536536d35
venelinpetrov <vpetrov33tu@gmail.com>
2019-09-01 19:04:15 +0300

    Refactoring

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { connect } from 'react-redux';
 import './App.css';
 import TodoForm from './components/TodoForm';
 import TodoList from './components/TodoList';
-import { bindActionCreators } from 'redux';
 import { updateCurrent } from './reducers/todo';
 
 const App = props => {
@@ -18,8 +17,8 @@ const App = props => {
     );
 }
 
-const mapDispatchToProps = dispatch => bindActionCreators({ updateCurrent }, dispatch);
-const mapStateToProps = state => state;
-const ConnectedApp = connect(mapStateToProps, mapDispatchToProps)(App);
 
-export default ConnectedApp;
+export default connect(
+    state => state,
+    { updateCurrent }
+)(App);
